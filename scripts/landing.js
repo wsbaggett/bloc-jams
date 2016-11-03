d112e69f05f8920d74d90029a6523df7cce50c6e
Walter S. Baggett <walterbaggett@legalshieldcorp.com>
2016-11-03 11:19:38 -0500

    checkpoint 9 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
  
-                 var points = document.getElementsByClassName('point');
-    
+ var animatePoints = function(points) {
                  var revealPoint = function(indNum) {
                      points[indNum].style.opacity = 1;
                      points[indNum].style.transform = "scaleX(1) translateY(0)";
@@ -12,5 +11,19 @@ var animatePoints = function() {
                  for (var i = 0; i<points.length; i++) {
                      revealPoint(i);
                  }
- 
              };
+
+window.onload = function() {
+    // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+    
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+     window.addEventListener('scroll', function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+ }
