aee424c62753ac406025ddcd670b4c6a144a8997
Walter S. Baggett <walterbaggett@legalshieldcorp.com>
2016-11-16 10:53:18 -0600

    checkpoint 17 complete

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,6 @@
-var collectionItemTemplate =
+// #1
+ var buildCollectionItemTemplate = function() {
+     var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -13,14 +15,20 @@ var collectionItemTemplate =
    + '  </div>'
    + '</div>'
    ;
-window.onload = function() {
-     // #1
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
-     // #2
-     collectionContainer.innerHTML = '';
- 
+      
+   // #2
+     return $(template);
+ };     
+
+$(window).load(function() {
      // #3
+     var $collectionContainer = $('.album-covers');
+     // #4
+     $collectionContainer.empty();
+    
      for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
+         var $newThumbnail = buildCollectionItemTemplate();
+         // #5
+         $collectionContainer.append($newThumbnail);
      }
- }
+ });
